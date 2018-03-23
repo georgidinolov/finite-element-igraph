dd30ae0358f1473f25d0810ba93615ef580e59c9
Georgi Dinolov <gdinolov@soe.ucsc.edu>
2018-03-22 19:45:28 -0700

    point checker done

--- kriging-test-fast.cpp
+++ kriging-test-fast.cpp
@@ -21,16 +21,15 @@
 #include <vector>
 
 int main(int argc, char *argv[]) {
-  if (argc < 5 || argc > 5) {
+  if (argc < 4 || argc > 4) {
     printf("You must provide input\n");
-    printf("The input is: \n\nnumber data points;\nrho;\ninput file for likelihoods;\nnumber_points_for_integration;\n");
+    printf("The input is: \n\nnumber of points of data;\nnumber of samples per datum;\ninput file for likelihoods;\n");
     exit(0);
   }
 
-  unsigned N = std::stoi(argv[1]);
-  double rho = std::stod(argv[2]);
+  unsigned number_points_of_data = std::stoi(argv[1]);
+  unsigned number_points_per_datum = std::stoi(argv[2]);
   std::string input_file_name = argv[3];
-  int M = std::stoi(argv[4]);
 
   static int counter = 0;
   static gsl_rng* r_ptr_threadprivate;
@@ -45,7 +44,7 @@ int main(int argc, char *argv[]) {
   gsl_rng_env_setup();
   Type = gsl_rng_default;
   r_ptr_local = gsl_rng_alloc(Type);
-  gsl_rng_set(r_ptr_local, seed_init + N);
+  gsl_rng_set(r_ptr_local, seed_init + number_points_of_data);
 
   int tid=0;
   unsigned i=0;
@@ -59,93 +58,125 @@ int main(int argc, char *argv[]) {
     printf("Thread %d: counter %d\n", tid, counter);
   }
 
-  std::vector<likelihood_point> points_for_kriging (0);
-  std::vector<likelihood_point> points_for_integration (M);
+  std::vector<std::vector<likelihood_point>> points_for_kriging_not_flipped (number_points_of_data);
+  std::vector<std::vector<likelihood_point>> points_for_kriging_flipped (number_points_of_data);
+  std::vector<likelihood_point> points_for_integration = std::vector<likelihood_point> (1);
+
+  std::ifstream input_file(input_file_name);
+  if (input_file.is_open()) {
+    for (i=0; i<number_points_of_data; ++i) {
+      for (unsigned m=0; m<number_points_per_datum; ++m) {
+	likelihood_point current_lp = likelihood_point();
+	input_file >> current_lp;
+
+	if (current_lp.FLIPPED) {
+	  points_for_kriging_flipped[i].push_back(current_lp);
+	} else {
+	  points_for_kriging_not_flipped[i].push_back(current_lp);
+	}
+
+      }
+      std::cout << points_for_kriging_not_flipped[i].size() << "\n";
+      std::cout << points_for_kriging_flipped[i].size() << "\n";
+      std::cout << std::endl;
+    }
+  }
+
+  std::vector<GaussianInterpolatorWithChecker> GPs_not_flipped(number_points_of_data);
+  std::vector<GaussianInterpolatorWithChecker> GPs_flipped(number_points_of_data);
 
   auto t1 = std::chrono::high_resolution_clock::now();
-#pragma omp parallel default(none) private(i) shared(points_for_integration, N, M, seed_init, rho)
+#pragma omp parallel default(none) private(i) shared(number_points_of_data, number_points_per_datum, seed_init, points_for_kriging_flipped, points_for_kriging_not_flipped, GPs_flipped, GPs_not_flipped, points_for_integration)
   {
 #pragma omp for
-    for (i=0; i<M; ++i) {
-      long unsigned seed = seed_init + N + i;
+    for (i=0; i<number_points_of_data; ++i) {
+      
+      if (points_for_kriging_not_flipped[i].size() > 6) {
+	GPs_not_flipped[i] = GaussianInterpolatorWithChecker(points_for_integration,
+							     points_for_kriging_not_flipped[i]);
+      } else {
+	GPs_not_flipped[i] = GaussianInterpolatorWithChecker(points_for_integration,
+							     points_for_kriging_flipped[i]);
+      }
+      GPs_not_flipped[i].optimize_parameters();
 
-      double log_sigma_x = 1 + gsl_ran_gaussian(r_ptr_threadprivate, 1.0);
-      double log_sigma_y = 1 + gsl_ran_gaussian(r_ptr_threadprivate, 1.0);
-
-      BrownianMotion BM = BrownianMotion(seed,
-					 10e6,
-					 rho,
-					 exp(log_sigma_x),
-					 exp(log_sigma_y),
-					 0.0,
-					 0.0,
-					 1.0);
-
-      points_for_integration[i] = BM;
+      if (points_for_kriging_flipped[i].size() > 6) {
+	GPs_flipped[i] = GaussianInterpolatorWithChecker(points_for_integration,
+							 points_for_kriging_flipped[i]);
+      } else {
+	GPs_flipped[i] = GaussianInterpolatorWithChecker(points_for_integration,
+							 points_for_kriging_not_flipped[i]);
+      }
+      GPs_flipped[i].optimize_parameters();
     }
   }
   auto t2 = std::chrono::high_resolution_clock::now();
-
-  std::cout << "Generating " << M << " points for integration took "
+  std::cout << "Generating " << number_points_of_data << " points for integration took "
 	    << std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count() << " seconds." << std::endl;
 
-  std::ifstream input_file(input_file_name);
+  for (i=0; i<number_points_of_data; ++i) {
+    std::string output_file_name = "optimal-parameters-data-point-" + std::to_string(i) + "-" +
+      "not-flipped-" +
+      input_file_name;
 
-  if (input_file.is_open()) {
-    for (i=0; i<N; ++i) {
-      likelihood_point current_lp = likelihood_point();
-      input_file >> current_lp;
-
-      if (i >= 0) { 
-	if (current_lp.FLIPPED) {
-	  points_for_kriging.push_back(current_lp); 
-	}
-      }
-
+    std::ofstream output_file(output_file_name);
+    if (output_file.is_open()) {
+      output_file << GPs_not_flipped[i].parameters;
     }
+    output_file.close();
+
+    output_file_name = "optimal-parameters-data-point-" + std::to_string(i) + "-" +
+      "flipped-" +
+      input_file_name;
+
+    output_file = std::ofstream(output_file_name);
+    if (output_file.is_open()) {
+      output_file << GPs_flipped[i].parameters;
+    }
+    output_file.close();
   }
+  
+  // parameters_nominal params = parameters_nominal();
+  // std::vector<double> L =
+  //   std::vector<double> {0.1,
+  // 			 0.0,0.1,
+  // 			 0.0,0.0,0.1,
+  // 			 0.0,0.0,0.0,0.1,
+  // 			 0.0,0.0,0.0,0.0,0.1,
+  // 			 0.0,0.0,0.0,0.0,0.0,0.1,
+  // 			 0.0,0.0,0.0,0.0,0.0,0.0,0.1};
 
-  parameters_nominal params = parameters_nominal();
-  std::vector<double> L =
-    std::vector<double> {0.1,
-			 0.0,0.1,
-			 0.0,0.0,0.1,
-			 0.0,0.0,0.0,0.1,
-			 0.0,0.0,0.0,0.0,0.1,
-			 0.0,0.0,0.0,0.0,0.0,0.1,
-			 0.0,0.0,0.0,0.0,0.0,0.0,0.1};
+  // params.lower_triag_mat_as_vec = L;
+  // params.sigma_2 = 0.001;
+  // params.phi = -10.01;
+  // params.tau_2 = 1.0;
 
-  params.lower_triag_mat_as_vec = L;
-  params.sigma_2 = 0.001;
-  params.phi = -10.01;
-  params.tau_2 = 1.0;
+  // std::cout << "points_for_kriging.size() = " << points_for_kriging.size() << std::endl;
+  // std::cout << "points_for_integration.size() = " << points_for_integration.size() << std::endl;
 
-  std::cout << "points_for_kriging.size() = " << points_for_kriging.size() << std::endl;
-  std::cout << "points_for_integration.size() = " << points_for_integration.size() << std::endl;
+  // GaussianInterpolator GP_prior = GaussianInterpolator(points_for_integration,
+  // 						       points_for_kriging,
+  // 						       params);
 
-  GaussianInterpolator GP_prior = GaussianInterpolator(points_for_integration,
-  						       points_for_kriging,
-  						       params);
+  // GP_prior.optimize_parameters();
+  // params = GP_prior.parameters;
+  // std::cout << "optimal parameters = \n" << params << std::endl;
 
-  GP_prior.optimize_parameters();
-  params = GP_prior.parameters;
-  std::cout << "optimal parameters = \n" << params << std::endl;
+  // double integral = 0;
+  // for (unsigned i=0; i<points_for_integration.size(); ++i) {
+  //   double add = GP_prior(points_for_integration[i]);
+  //   integral = integral +
+  //     1.0/M * add;
+  // }
+  // std::cout << "Integral = " << integral << std::endl;
 
-  double integral = 0;
-  for (unsigned i=0; i<points_for_integration.size(); ++i) {
-    double add = GP_prior(points_for_integration[i]);
-    integral = integral +
-      1.0/M * add;
-  }
-  std::cout << "Integral = " << integral << std::endl;
+  // std::string output_file_name = "optimal-parameters-" + std::to_string(N) + "-" + input_file_name;
+  // std::ofstream output_file(output_file_name);
 
-  std::string output_file_name = "optimal-parameters-" + std::to_string(N) + "-" + input_file_name;
-  std::ofstream output_file(output_file_name);
-
-  if (output_file.is_open()) {
-    output_file << params;
-  }
-  output_file.close();
+  // if (output_file.is_open()) {
+  //   output_file << params;
+  // }
+  // output_file.close();
 
   return 0;
 }
